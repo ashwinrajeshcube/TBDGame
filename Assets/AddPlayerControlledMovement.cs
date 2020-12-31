7c9fa8f5ad01209e7f70e67f3322144263cd3f80
Ashwin Rajesh <ashwinrajeshcube@gmail.com>
2020-12-30 20:59:14 -0500

    Original Project

---
+++ Assets/AddPlayerControlledMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class AddPlayerControlledMovement : MonoBehaviour
+{
+	[SerializeField]
+Vector3 v3;
+	[SerializeField]
+KeyCode keyPositive;
+	[SerializeField]
+KeyCode keyNegative;
+
+	[SerializeField]
+Vector3 v32;
+	[SerializeField]
+KeyCode keyPositive2;
+	[SerializeField]
+KeyCode keyNegative2;
+void FixedUpdate(){
+	if(Input.GetKey(keyPositive)){
+	GetComponent<Rigidbody>().velocity+=v3;}
+	if(Input.GetKey(keyNegative)){
+	GetComponent<Rigidbody>().velocity-=v3;}
+	if(Input.GetKey(keyPositive2)){
+	GetComponent<Rigidbody>().velocity+=v32;}
+	if(Input.GetKey(keyNegative2)){
+	GetComponent<Rigidbody>().velocity-=v32;}
+}
+}
+
---
+++ Assets/RotateCam.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCam : MonoBehaviour
+{
+	[SerializeField] private float degreeStep = 30f;
+    [SerializeField] private Transform right;
+	[SerializeField]
+	KeyCode keyPositive;
+	[SerializeField]
+	KeyCode keyNegative;
+    void Update()
+    {
+		float step = degreeStep * Time.deltaTime;
+		if(Input.GetKey(keyPositive))
+        right.Rotate(Vector3.up, step); 
+		if(Input.GetKey(keyNegative))
+		right.Rotate(Vector3.down, step);
+		
+    }
+}
